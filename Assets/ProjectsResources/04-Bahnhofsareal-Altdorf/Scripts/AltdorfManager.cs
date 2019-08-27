f667abf76d62a17c19faf16b3b85fe54bbb8a560
Charrière Loïc <loic.charriere@students.bfh.ch>
2019-08-27 14:51:04 +0200

    Unity 2019.2.2f1

--- Assets/ProjectsResources/04-Bahnhofsareal-Altdorf/Scripts/AltdorfManager.cs
+++ Assets/ProjectsResources/04-Bahnhofsareal-Altdorf/Scripts/AltdorfManager.cs
@@ -9,29 +9,32 @@ public class AltdorfManager : MonoBehaviour
 {
     #region PUBLIC_MEMBER_VARIABLES
 
+    public Transform alternativesAR;
+    public Transform alternativesVR;
+
     #endregion // PUBLIC_MEMBER_VARIABLES
 
 
     #region PRIVATE_MEMBER_VARIABLES
+
+    private int m_currentAlternativeID = 0;
+    private bool m_vegetationEnabled = true;
+    private GameObject[] m_vegetationTag;
 
     #endregion // PRIVATE_MEMBER_VARIABLES
 
 
     #region MONOBEHAVIOUR_METHODS
 
-    protected void Awake()
-    {
-        StartCoroutine(UtilityHelper.LoadDevice("Vuforia"));
-    }
-
     protected void Start()
     {
-        Debug.Log("VERSION: " + Application.version);
+        m_vegetationTag = GameObject.FindGameObjectsWithTag("Vegetation");
     }
 
     protected void Update()
     {
-
+        UpdateAlternatives();
+        UpdateVegetation();
     }
 
     #endregion // MONOBEHAVIOUR_METHODS
@@ -39,5 +42,38 @@ public class AltdorfManager : MonoBehaviour
 
     #region PUBLIC_METHODS
 
+    /*<summary>
+     * Switch to the next project in the list.
+     * </summary>
+     * */
+    public void SwitchProject()
+    {
+        m_currentAlternativeID = (m_currentAlternativeID + 1) % 2;
+    }
+
+    public void EnableVegetation(bool value)
+    {
+        m_vegetationEnabled = value;
+    }
+
     #endregion // PUBLIC_METHODS
+
+
+    #region PRIVATE_METHODS
+
+    private void UpdateAlternatives()
+    {
+        foreach (Transform child in alternativesAR) child.gameObject.SetActive(false);
+        foreach (Transform child in alternativesVR) child.gameObject.SetActive(false);
+
+        alternativesAR.GetChild(m_currentAlternativeID).gameObject.SetActive(true);
+        alternativesVR.GetChild(m_currentAlternativeID).gameObject.SetActive(true);
+    }
+
+    private void UpdateVegetation()
+    {
+        foreach (GameObject go in m_vegetationTag) go.SetActive(m_vegetationEnabled);
+    }
+
+    #endregion // PRIVATE_METHODS
 }
